2926333549b5b41e49cb92b8b163d80465b17d12
Cooler Zhang <cooler.zhang@outlook.com>
2018-11-17 23:25:20 +0800

    Initialize Solution

---
+++ YAMLTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using YamlDotNet;
+
+namespace YAMLTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begin Test Yaml........................");
+            Console.WriteLine();
+
+            var user = new User()
+            {
+                Id = 1,
+                Email = "abc@a.com",
+                UserName = "admin",
+                Password = "123456",
+                IsActive = true,
+                Person = new Person()
+                {
+                    Id = 1,
+                    Name = "张三",
+                    Age = 30
+                }
+            };
+
+            Console.WriteLine("User Hash:" + user.GetHashCode());
+
+            var serializer = new YamlDotNet.Serialization.Serializer();
+
+            var yamlResult = serializer.Serialize(user);
+            Console.WriteLine(yamlResult);
+
+            var deserializer = new YamlDotNet.Serialization.Deserializer();
+            var userClone = deserializer.Deserialize<User>(yamlResult);
+
+            Console.WriteLine("UserClone Hash:" + userClone.GetHashCode());
+
+            Console.WriteLine("User equals UserClone:" + (user == userClone));
+
+
+            Console.WriteLine();
+
+            Console.WriteLine("End Test Yaml........................");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ YAMLTest/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YAMLTest
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public Person Person { get; set; }
+    }
+}
